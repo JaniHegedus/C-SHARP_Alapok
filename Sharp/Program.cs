5adcbe1868e9af7cd22d46f23f308c6c1bcaf0ef
JaniHegedus <90450741+JaniHegedus@users.noreply.github.com>
2023-02-10 14:45:06 +0100

    init

---
+++ Sharp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    private static void main(string[] args)
+    {
+        Console.WriteLine("Hello, World");
+    }
+}
